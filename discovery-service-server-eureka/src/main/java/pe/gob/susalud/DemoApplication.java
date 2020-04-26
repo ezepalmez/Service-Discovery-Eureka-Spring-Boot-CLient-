df5395e9c2fb56e88601f080219b7dae02748de3
ezepalmez <ezepalmez@gmail.com>
2020-04-26 11:36:27 -0500

    Initial commit

---
+++ discovery-service-server-eureka/src/main/java/pe/gob/susalud/DemoApplication.java
@@ -0,0 +1,15 @@
+package pe.gob.susalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
